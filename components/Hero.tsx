ad62becb59eb9cacf4e695137bb94df71463fd2f
Rahul <rahulbt2016@gmail.com>
2023-03-03 05:12:30 -0500

    Added header and hero sections

---
+++ components/Hero.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Link from 'next/link'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles';
+
+type Props = {}
+
+function Hero({}: Props) {
+    const [text, count] = useTypewriter({
+        words: [
+            'Hi, The Name\'s Rahul Tiwari', 
+            'Guy-who-loves-Music.tsx', 
+            '<ButLovesToCodeMore />'],
+            loop: true,
+            delaySpeed: 2000,
+    });
+  return (
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+        <BackgroundCircles />
+        <img 
+            className='relative rounded-full h-32 w-32 mx-auto object-cover'
+            src="images/rahul_image_1.png" alt="" />
+        <div className='z-20'>
+            <h2 className='font-bold text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
+                FULL STACK DEVELOPER
+            </h2>
+            <h1 className='text-5xl font-semibold lg:text-6xl px-10'>
+                <span className='mr-3'>{text}</span>
+                <Cursor cursorColor='#F7AB0A' />
+            </h1>
+
+            <div className='pt-5'>
+                <Link href='#about'>
+                    <button className='heroButton'>About</button>
+                </Link>
+                <Link href='#experience'>
+                    <button className='heroButton'>Experience</button>
+                </Link>
+                <Link href='#skills'>
+                    <button className='heroButton'>Skills</button>
+                </Link>
+                <Link href='#projects'>
+                    <button className='heroButton'>Projects</button>
+                </Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
